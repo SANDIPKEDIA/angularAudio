cc5ba586066fccba8c642d0a7b56a7f1afbce4cc
SANDIPKEDIA <47380324+SANDIPKEDIA@users.noreply.github.com>
2021-01-06 01:04:32 +0530

    Update app.component.ts
    add stop function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.playAudio();
-    
+  ngOnInit() : void {
+    this.playAudio(); 
   }
-
-  playAudio(){
-    let audio = new Audio();
-    audio.src = "../../../assets/audio/snapchat_ringtone.mp3";
-    audio.load();
-    audio.play();
-  }
-  
-
+  private audioObj = new Audio();
+  playAudio(){ 
+    this.audioObj.src = "../../../assets/audio/Jeremy Zucker - comethru.mp3";
+    this.audioObj.load();
+    this.audioObj.play();
+   }
+  stopAudio(){
+    this.audioObj.pause();
+  }  
 }
